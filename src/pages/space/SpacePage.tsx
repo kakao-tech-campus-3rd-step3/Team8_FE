64db54babee28e55c2218fb903666bc76c28880d
Moderator11 <p7925221@gmail.com>
2025-08-27 16:27:51 +0900

    feat: 나의 스페이스 기본 골격
    - 프로파일 더미 컴포넌트 추가
    - 플랜스페이스 더미 컴포넌트 추가
    - 나의 스페이스 페이지 레이아웃 설정

--- src/pages/space/SpacePage.tsx
+++ src/pages/space/SpacePage.tsx
@@ -1,5 +1,27 @@
+import TopBar from '@/components/TopBar';
+import Profile from './components/Profile';
+import PlanSpace from './components/PlanSpace';
+import styled from 'styled-components';
+
 function SpacePage() {
-  return <>Personal Space + My Page</>;
+  return (
+    <>
+      <TopBar>나의 스페이스</TopBar>
+      <SpacePageWrapper>
+        <Profile />
+        <PlanSpace />
+      </SpacePageWrapper>
+    </>
+  );
 }
+
+const SpacePageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+
+  padding: 24px;
+`;
 
 export default SpacePage;
---
+++ src/pages/space/components/Profile.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+function Profile() {
+  return <ProfileWrapper>프로파일</ProfileWrapper>;
+}
+
+const ProfileWrapper = styled.div`
+  width: 100%;
+  border: 1px solid black;
+`;
+
+export default Profile;
