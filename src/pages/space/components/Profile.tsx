8ad223b320909c853258633bec0b65d907d230e3
Moderator11 <p7925221@gmail.com>
2025-08-27 17:46:27 +0900

    feat: Profile 컴포넌트 레이아웃
    - API로 받아올 유저 정보를 dummyProfileResponse 객체로 대신하였습니다
    - Profile 컴포넌트의 반응형 UI가 깨지는 해상도 가로 픽셀 하한선은 430px 입니다

--- src/pages/space/components/Profile.tsx
+++ src/pages/space/components/Profile.tsx
@@ -1,12 +1,127 @@
+import { colorSystem } from '@/styles/colorSystem';
+import { fontSystem } from '@/styles/fontSystem';
 import styled from 'styled-components';
 
+const dummyProfileResponse = {
+  status: 200,
+  message: '내 정보를 불러왔습니다.',
+  member: {
+    id: 1234,
+    email: 'example@test.com',
+    name: '홍길동',
+    contact: '+82-10-1234-1234',
+    mbti: 'ISTP',
+  },
+};
+
 function Profile() {
-  return <ProfileWrapper>프로파일</ProfileWrapper>;
+  const member = dummyProfileResponse.member;
+
+  return (
+    <ProfileWrapper>
+      <HorizontalLayout>
+        <VerticalLayout>
+          <ImagePlaceholder />
+          <EditPictureButton>사진 수정</EditPictureButton>
+        </VerticalLayout>
+        <InfoSection>
+          <Entry>
+            <Section>이메일</Section>
+            <div>{member.email}</div>
+          </Entry>
+          <Entry>
+            <Section>이름</Section>
+            <div>{member.name}</div>
+          </Entry>
+          <Entry>
+            <Section>연락처</Section>
+            <div>{member.contact}</div>
+          </Entry>
+          <Entry>
+            <Section>MBTI</Section>
+            <div>{member.mbti}</div>
+          </Entry>
+          <EditInfoButton>수정</EditInfoButton>
+        </InfoSection>
+      </HorizontalLayout>
+    </ProfileWrapper>
+  );
 }
 
+//==============<사진 섹션>==============
+
+const VerticalLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+`;
+
+const ImagePlaceholder = styled.div`
+  width: 128px;
+  height: 128px;
+  border-radius: 64px;
+  background-color: ${colorSystem.tertiary_white._400};
+`;
+
+const EditPictureButton = styled.button`
+  color: ${colorSystem.primary_yellow._400};
+  border: none;
+  background-color: transparent;
+`;
+
+//==============<사진 섹션>==============
+
+//==============<정보 섹션>==============
+
+const EditInfoButton = styled.button`
+  margin-top: 16px;
+  background-color: ${colorSystem.primary_yellow._300};
+
+  border: none;
+  border-radius: 40px;
+
+  width: 80px;
+  padding: 12px;
+  align-self: flex-end;
+`;
+
+const Entry = styled.div`
+  display: flex;
+`;
+
+const Section = styled.div`
+  ${fontSystem.body.medium}
+  font-weight: bold;
+  min-width: 52px;
+`;
+
+const InfoSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+//==============<정보 섹션>==============
+
+const HorizontalLayout = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  gap: 16px;
+`;
+
+// 반응형 깨지는 해상도 가로 픽셀 하한선: 430px
 const ProfileWrapper = styled.div`
-  width: 100%;
-  border: 1px solid black;
+  min-width: calc(100% - 2 * 40px); // 좌우 padding 값
+  padding: 40px;
+
+  display: flex;
+  flex-direction: column;
+  border-radius: 40px;
+  box-shadow: 0 2px 12px rgba(255, 192, 77, 0.3);
 `;
 
 export default Profile;
