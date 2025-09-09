66d28d48b3aabaea26dfb97d12b786e770e21f7a
whyyhyh <judynam03@gmail.com>
2025-09-09 16:46:01 +0900

    fix: 다른 페이지 접속 불가 오류 해결
    새로운 컴포넌트 RootRedirect 만들어 홈으로 리다이렉트

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -6,7 +6,7 @@ import RegisterPage from '@pages/register/RegisterPage';
 import SpacePage from '@pages/space/SpacePage';
 import RoutingPanel from '@components/RoutingPanel';
 import { PATH } from '@utils/path';
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import ColorPanel from './ColorPanel';
 import FontPanel from './FontPanel';
 import { ToastContainer } from 'react-toastify';
@@ -14,36 +14,18 @@ import WayPointPage from '@/pages/plan/subpages/WayPointPage';
 import TravelerPage from '@/pages/plan/subpages/TravelerPage';
 import MapPage from '@/pages/plan/subpages/MapPage';
 import MemoPage from '@/pages/plan/subpages/MemoPage';
-import { useEffect } from 'react';
 
-function AppRoutes() {
-  const navigate = useNavigate();
+function RootRedirect() {
+  const isFirstVisit = localStorage.getItem('isFirstVisit');
 
-  useEffect(() => {
-    const isFirstVisit = localStorage.getItem('isFirstVisit');
-    if (isFirstVisit === null) {
-      localStorage.setItem('isFirstVisit', 'false');
-      navigate(PATH.LANDING);
-    } else {
-      navigate(PATH.HOME);
-    }
-  }, [navigate]);
-
-  return (
-    <Routes>
-      <Route path={PATH.LANDING} element={<LandingPage />} />
-      <Route path={PATH.HOME} element={<HomePage />} />
-      <Route path={PATH.LOGIN} element={<LoginPage />} />
-      <Route path={PATH.REGISTER} element={<RegisterPage />} />
-      <Route path={PATH.SPACE} element={<SpacePage />} />
-      <Route path={PATH.PLAN.BASE} element={<PlanPage />}>
-        <Route path={PATH.PLAN.WAYPOINT} element={<WayPointPage />} />
-        <Route path={PATH.PLAN.TRAVELER} element={<TravelerPage />} />
-        <Route path={PATH.PLAN.MAP} element={<MapPage />} />
-        <Route path={PATH.PLAN.MEMO} element={<MemoPage />} />
-      </Route>
-    </Routes>
-  );
+  if (isFirstVisit === null) {
+    // 첫 방문이면 localStorage에 기록하고 LandingPage 렌더링
+    localStorage.setItem('isFirstVisit', 'false');
+    return <LandingPage />;
+  } else {
+    // 재방문이면 HomePage로 리다이렉트
+    return <Navigate to={PATH.HOME} replace />;
+  }
 }
 
 function Router() {
@@ -53,7 +35,22 @@ function Router() {
       <ColorPanel />
       <FontPanel />
       <ToastContainer />
-      <AppRoutes />
+      <Routes>
+        {/* 2. 루트 경로('/')에 RootRedirect 컴포넌트를 연결 */}
+        <Route path={PATH.LANDING} element={<RootRedirect />} />
+        
+        {/* 나머지 라우트는 그대로 유지 */}
+        <Route path={PATH.HOME} element={<HomePage />} />
+        <Route path={PATH.LOGIN} element={<LoginPage />} />
+        <Route path={PATH.REGISTER} element={<RegisterPage />} />
+        <Route path={PATH.SPACE} element={<SpacePage />} />
+        <Route path={PATH.PLAN.BASE} element={<PlanPage />}>
+          <Route path={PATH.PLAN.WAYPOINT} element={<WayPointPage />} />
+          <Route path={PATH.PLAN.TRAVELER} element={<TravelerPage />} />
+          <Route path={PATH.PLAN.MAP} element={<MapPage />} />
+          <Route path={PATH.PLAN.MEMO} element={<MemoPage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
