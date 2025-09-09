1dd3dc15783a58a499953168e89c89a590ba3b07
whyyhyh <judynam03@gmail.com>
2025-09-09 16:12:32 +0900

    feat: 최초 접속시에만 랜딩페이지 표시
    useEffect 훅을 사용하여 컴포넌트가 마운트될 때 localStorage를 사용하여 방문 기록을 확인하고, 이미 방문한 사용자라면 홈페이지로 리다이렉트하도록 수정.

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -6,7 +6,7 @@ import RegisterPage from '@pages/register/RegisterPage';
 import SpacePage from '@pages/space/SpacePage';
 import RoutingPanel from '@components/RoutingPanel';
 import { PATH } from '@utils/path';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import ColorPanel from './ColorPanel';
 import FontPanel from './FontPanel';
 import { ToastContainer } from 'react-toastify';
@@ -14,6 +14,37 @@ import WayPointPage from '@/pages/plan/subpages/WayPointPage';
 import TravelerPage from '@/pages/plan/subpages/TravelerPage';
 import MapPage from '@/pages/plan/subpages/MapPage';
 import MemoPage from '@/pages/plan/subpages/MemoPage';
+import { useEffect } from 'react';
+
+function AppRoutes() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const isFirstVisit = localStorage.getItem('isFirstVisit');
+    if (isFirstVisit === null) {
+      localStorage.setItem('isFirstVisit', 'false');
+      navigate(PATH.LANDING);
+    } else {
+      navigate(PATH.HOME);
+    }
+  }, [navigate]);
+
+  return (
+    <Routes>
+      <Route path={PATH.LANDING} element={<LandingPage />} />
+      <Route path={PATH.HOME} element={<HomePage />} />
+      <Route path={PATH.LOGIN} element={<LoginPage />} />
+      <Route path={PATH.REGISTER} element={<RegisterPage />} />
+      <Route path={PATH.SPACE} element={<SpacePage />} />
+      <Route path={PATH.PLAN.BASE} element={<PlanPage />}>
+        <Route path={PATH.PLAN.WAYPOINT} element={<WayPointPage />} />
+        <Route path={PATH.PLAN.TRAVELER} element={<TravelerPage />} />
+        <Route path={PATH.PLAN.MAP} element={<MapPage />} />
+        <Route path={PATH.PLAN.MEMO} element={<MemoPage />} />
+      </Route>
+    </Routes>
+  );
+}
 
 function Router() {
   return (
@@ -22,19 +53,7 @@ function Router() {
       <ColorPanel />
       <FontPanel />
       <ToastContainer />
-      <Routes>
-        <Route path={PATH.LANDING} element={<LandingPage />} />
-        <Route path={PATH.HOME} element={<HomePage />} />
-        <Route path={PATH.LOGIN} element={<LoginPage />} />
-        <Route path={PATH.REGISTER} element={<RegisterPage />} />
-        <Route path={PATH.SPACE} element={<SpacePage />} />
-        <Route path={PATH.PLAN.BASE} element={<PlanPage />}>
-          <Route path={PATH.PLAN.WAYPOINT} element={<WayPointPage />} />
-          <Route path={PATH.PLAN.TRAVELER} element={<TravelerPage />} />
-          <Route path={PATH.PLAN.MAP} element={<MapPage />} />
-          <Route path={PATH.PLAN.MEMO} element={<MemoPage />} />
-        </Route>
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
