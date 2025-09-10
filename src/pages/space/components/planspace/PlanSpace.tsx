48728f9967f8e025cba55c870c7b0217f1dde768
Moderator11 <p7925221@gmail.com>
2025-09-10 13:09:53 +0900

    feat: 계획 추가하기 버튼 추가
    - 계획 페이지에 추가하기 버튼 추가

--- src/pages/space/components/planspace/PlanSpace.tsx
+++ src/pages/space/components/planspace/PlanSpace.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import type { PlanType } from '@/pages/space/types/plan';
 import PlanCard from './PlanCard';
+import Add from '@/assets/icons/Add';
 
 const dummyPlanResponse = {
   status: 200,
@@ -32,14 +33,34 @@ const dummyPlanResponse = {
 function PlanSpace() {
   const plans = dummyPlanResponse.plans;
   // 가장 최근 수정한 플랜을 highlight하면 어떻까요?
+  const handleAddNewPlan = () => {
+    console.log('새 계획 생성 API 호출');
+  };
+
   return (
     <PlanSpaceWrapper>
       {plans.map((plan: PlanType) => {
         return <PlanCard key={plan.id} plan={plan} highlight={plan.id === 1234} />;
       })}
+      <AddButton onClick={handleAddNewPlan}>
+        <Add />새 여행 계획하기
+      </AddButton>
     </PlanSpaceWrapper>
   );
 }
+
+const AddButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 20px;
+
+  border-radius: 16px;
+  box-shadow: 0 2px 12px rgba(255, 192, 77, 0.3);
+
+  cursor: pointer;
+`;
 
 const PlanSpaceWrapper = styled.div`
   display: grid;
