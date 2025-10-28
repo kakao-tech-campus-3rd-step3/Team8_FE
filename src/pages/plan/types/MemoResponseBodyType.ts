c461a737576a6411ec658a10d7852c0963f7a6dc
Moderator11 <p7925221@gmail.com>
2025-10-28 22:53:06 +0900

    feat: 공통 sessionId 필드 삽입

--- src/pages/plan/types/MemoResponseBodyType.ts
+++ src/pages/plan/types/MemoResponseBodyType.ts
@@ -1,19 +1,20 @@
 import type { MemoData } from '../flow/canvasComponents/Memo';
+import type { BaseResponseBodyType } from './baseResponseBodyType';
 
 export type MemoResponseType =
-  | {
+  | (BaseResponseBodyType & {
       type: 'INIT';
       MEMO: MemoData[];
-    }
-  | {
+    })
+  | (BaseResponseBodyType & {
       type: 'CREATE';
       MEMO: MemoData;
-    }
-  | {
+    })
+  | (BaseResponseBodyType & {
       type: 'UPDATE';
       MEMO: MemoData;
-    }
-  | {
+    })
+  | (BaseResponseBodyType & {
       type: 'DELETE';
       MEMO: number;
-    };
+    });
--- src/pages/plan/types/WaypointResponseBodyType.ts
+++ src/pages/plan/types/WaypointResponseBodyType.ts
@@ -1,19 +1,20 @@
 import type { WaypointData } from '../flow/canvasComponents/Waypoint';
+import type { BaseResponseBodyType } from './baseResponseBodyType';
 
 export type WayPointResponseType =
-  | {
+  | (BaseResponseBodyType & {
       type: 'INIT';
       WAYPOINT: WaypointData[];
-    }
-  | {
+    })
+  | (BaseResponseBodyType & {
       type: 'CREATE';
       WAYPOINT: WaypointData;
-    }
-  | {
+    })
+  | (BaseResponseBodyType & {
       type: 'UPDATE';
       WAYPOINT: WaypointData;
-    }
-  | {
+    })
+  | (BaseResponseBodyType & {
       type: 'DELETE';
       WAYPOINT: number;
-    };
+    });
