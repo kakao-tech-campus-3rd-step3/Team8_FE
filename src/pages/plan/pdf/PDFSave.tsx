ff16efe0898e8b5a5d003c5c1dd4421dbf8396f0
Moderator11 <p7925221@gmail.com>
2025-10-31 02:34:09 +0900

    feat: PDF 다운로드 모달 수정

--- src/pages/plan/pdf/PDFSave.tsx
+++ src/pages/plan/pdf/PDFSave.tsx
@@ -1,14 +1,33 @@
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import PDFTemplate from './PDFTemplate';
 import { schedules, travelers } from './data/example';
+import styled from 'styled-components';
+import { colorSystem } from '@/styles/colorSystem';
 
-export default function PDFSave() {
+export default function PDFSave({ title }: { title: string }) {
   return (
     <PDFDownloadLink
       document={<PDFTemplate travelers={travelers} schedules={schedules} />}
-      fileName="예시계획.pdf"
+      fileName={`${title}-JourneyPlanner.pdf`}
+      style={{ textDecoration: 'none' }}
     >
-      {({ loading }) => (loading ? '문서를 생성중입니다...' : 'PDF 다운로드')}
+      {({ loading }) =>
+        loading ? (
+          <ExportButton disabled>PDF 생성중...</ExportButton>
+        ) : (
+          <ExportButton>PDF 다운로드</ExportButton>
+        )
+      }
     </PDFDownloadLink>
   );
 }
+
+const ExportButton = styled.button`
+  flex: 1;
+  padding: 16px;
+  border-radius: 8px;
+  border: none;
+  background-color: ${colorSystem.primary_yellow._500};
+  transition: all 0.25s;
+  cursor: pointer;
+`;
