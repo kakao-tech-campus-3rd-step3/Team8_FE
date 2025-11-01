d73cbe801571ee6a353896387dc71b8b33d3213b
Moderator11 <p7925221@gmail.com>
2025-11-01 21:43:39 +0900

    feat: 여행자 정보 fetch 로직

--- src/api/types/planDetail.ts
+++ src/api/types/planDetail.ts
@@ -1,16 +1,10 @@
+import type { TravelerType } from './traveler';
+
 export type PlanDetailType = {
   id: number;
   title: string;
   description: string;
   startDate: string;
   endDate: string;
-  travelers: {
-    id: number;
-    memberId: number;
-    name: string;
-    contact: string;
-    mbtiType: string;
-    role: string;
-    status: string;
-  }[];
+  travelers: TravelerType[];
 };
