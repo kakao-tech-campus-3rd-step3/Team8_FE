8725bc3c0133b67b89e0fa394182b819a5d0cf53
Moderator11 <p7925221@gmail.com>
2025-11-07 03:11:28 +0900

    invitation 초대 수락시 data 삭제

--- src/pages/inbox/hooks/useAcceptInvitation.ts
+++ src/pages/inbox/hooks/useAcceptInvitation.ts
@@ -12,9 +12,8 @@ const acceptInvitation = async (invitation: Invitation) => {
 export const useAcceptInvitation = () => {
   return useMutation({
     mutationFn: acceptInvitation,
-    onSuccess: (data) => {
+    onSuccess: () => {
       toast.success('초대 수락에 성공하였습니다.');
-      console.log(data);
     },
     onError: (error) => {
       toast.error('초대 수락에 실패하였습니다.');
