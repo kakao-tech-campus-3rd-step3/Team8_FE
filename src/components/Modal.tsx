6c299f5e12792624a9bd83eb02e7691a114021e2
Moderator11 <p7925221@gmail.com>
2025-11-06 18:44:00 +0900

    fix: 모달 오류 수정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,71 +1,24 @@
-// import { useBlockScroll } from '@/hooks/useBlockScroll';
-// import styled from 'styled-components';
-
-// function Modal({ children }: { children: React.ReactNode }) {
-//   useBlockScroll();
-//   return <ModalWrapper>{children}</ModalWrapper>;
-// }
-
-// const ModalWrapper = styled.div`
-//   width: 100vw;
-//   height: 100vh;
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-
-//   background-color: rgba(0, 0, 0, 0.5);
-// `;
-
-// export default Modal;
-import { type ReactNode } from 'react';
-import styled from 'styled-components';
 import { useBlockScroll } from '@/hooks/useBlockScroll';
-
-interface ModalProps {
-  children: ReactNode;
-import { createPortal } from 'react-dom';
+import styled from 'styled-components';
 
 function Modal({ children }: { children: React.ReactNode }) {
   useBlockScroll();
-  return createPortal(<ModalWrapper>{children}</ModalWrapper>, document.body);
+  return <ModalWrapper>{children}</ModalWrapper>;
 }
 
-const Modal = ({ children }: ModalProps) => {
-  useBlockScroll(); // 팝업 띄울 시 스크롤 방지
-
-  return (
-    <Dimmed>
-      <ModalContainer>{children}</ModalContainer>
-    </Dimmed>
-  );
-};
-
-export default Modal;
-
-const Dimmed = styled.div`
+const ModalWrapper = styled.div`
+  z-index: 1;
+  width: 100vw;
+  height: 100vh;
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.4);
 
-  /* --- (수정) 100 -> 1100 (혹은 2000 등 더 높은 값) --- */
-  z-index: 1100;
-  /* ------------------------------------------- */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: rgba(0, 0, 0, 0.5);
 `;
 
-const ModalContainer = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-
-  /* --- (수정) 101 -> 1101 (혹은 2001 등 더 높은 값) --- */
-  z-index: 1101;
-  /* ------------------------------------------- */
-`;
+export default Modal;
